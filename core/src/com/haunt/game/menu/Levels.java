13eafee9bd347fd1a974a1231474fd60290f1f95
Devin <superdevin00@gmail.com>
2023-11-26 04:19:19 -0600

    Level Added + Window sprite fix

--- core/src/com/haunt/game/menu/Levels.java
+++ core/src/com/haunt/game/menu/Levels.java
@@ -14,7 +14,8 @@ public class Levels {
     public Levels(HauntGame game) {
         this.game = game;
         levels = new LevelInfo[] { new LevelInfo("Level1.csv", 20, 0),
-                /*new LevelInfo("Level2.csv", 10, 5),
+
+                new LevelInfo("Level2.csv", 10, 5),
                 new LevelInfo("LevelVase.csv", 10, 5),
                 new LevelInfo("LevelSpike.csv", 5, 5),
                 new LevelInfo("LevelTowerSaws.csv", 5, 5),
@@ -25,11 +26,13 @@ public class Levels {
                 new LevelInfo("LevelSawSilo.csv", 5, 3),
                 new LevelInfo("LevelPyramid.csv", 5, 3),
                 new LevelInfo("LevelTowerFall.csv", 2, 2),
+                new LevelInfo("LevelWeaveRedux.csv", 3, 2),
                 new LevelInfo("LevelWiggly.csv", 5, 3),
                 new LevelInfo("LevelCircle.csv", 10, 3),
-                new LevelInfo("LevelTowerRedux.csv", 3, 3),*/
+                new LevelInfo("LevelTowerRedux.csv", 3, 3),
                 new LevelInfo("LevelDoubleBack.csv", 10, 3),
                 new LevelInfo("LevelInfinityRedux.csv", 10, 3),
+                
 
         };
         setLevel(0);
